1395d50c827ac2016c3a9ee477c981130cb72892
Grek Maria <grekmariaa@gmail.com>
2022-10-18 12:13:59 +0300

    used localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,27 @@ class App extends Component {
     state = {
         filter: '',
         contacts: [
-            { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-            { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-            { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-            { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+            // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+            // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+            // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+            // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
         ],
     };
+
+    componentDidMount() {
+        const contacts = localStorage.getItem('contacts');
+        const parsedContacts = JSON.parse(contacts);
+
+        if (parsedContacts) {
+            this.setState({ contacts: parsedContacts });
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.contacts !== prevState.contact) {
+            localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+        }
+    }
 
     formSubmitHemdler = ({ name, number }) => {
         const newContact = { id: nanoid(), name: name, number: number };
